39c1edc4d87fca7916a633f119c68d77e0cae312
EternalAzure <kall.miika@gmail.com>
2022-01-24 14:45:55 +0200

    new index response, working version of extended game demo, renamed route methods

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/01")
 def index():
-    return render_template("index.html")
+    return "Navigate games by adding '/<int>' to this web address eg. eternalazure-web-hosting.herokuapp.com/01"
 
-@app.route("/new")
-def new():
-    return render_template("new_index.html")
+@app.route("/01")
+def first():
+    return render_template("index01.html")
+
+@app.route("/02")
+def second():
+    return render_template("index02.html")
