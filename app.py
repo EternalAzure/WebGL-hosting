03cf4a38ebcf9209b10905c17db2c5b6183fe97a
EternalAzure <kall.miika@gmail.com>
2022-01-24 14:48:46 +0200

    fixed path in index route

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/01")
+@app.route("/")
 def index():
     return "Navigate games by adding '/<int>' to this web address eg. eternalazure-web-hosting.herokuapp.com/01"
 
